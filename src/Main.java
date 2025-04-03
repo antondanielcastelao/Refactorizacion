32e657619201f40d62ab629aaa5d3de685bfa410
LINKMASTER\conguchu <anton.davila.carro@gmail.com>
2025-04-03 22:13:30 +0200

    codigo refactorizado

--- src/Main.java
+++ src/Main.java
@@ -3,62 +3,62 @@ public class Main {
      * metodo para devolver la puntuacion de tenis
      */
     public static String getScore(int m_score1, int m_score2) {
-        String score = "";
-        int tempScore=0;
+
+        StringBuilder score = new StringBuilder();
 
         if (m_score1 == m_score2) {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+            score = new StringBuilder(switch (m_score1) {
+                case 0 -> "Love-All";
+                case 1 -> "Fifteen-All";
+                case 2 -> "Thirty-All";
+                case 3 -> "Forty-All";
+                default -> "Deuce";
+            });
 
-            }
-        }
-        else if (m_score1 >=4 || m_score2 >=4)
-        {
+        } else if (m_score1 >=4 || m_score2 >=4) {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+
+            if (minusResult==1) {
+                score = new StringBuilder("Advantage player1");
+            }
+            else if (minusResult ==-1) {
+                score = new StringBuilder("Advantage player2");
+            }
+            else if (minusResult>=2) {
+                score = new StringBuilder("Win for player1");
+            }
+            else {
+                score = new StringBuilder("Win for player2");
+            }
+        } else {
+            int tempScore;
+
+            for (int i=1; i<3; i++) {
+                if (i==1) {
+                    tempScore = m_score1;
+                } else {
+                    score.append("-");
+                    tempScore = m_score2;
+                }
+
+                switch(tempScore) {
                     case 0:
-                        score+="Love";
+                        score.append("Love");
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score.append("Fifteen");
                         break;
                     case 2:
-                        score+="Thirty";
+                        score.append("Thirty");
                         break;
                     case 3:
-                        score+="Forty";
+                        score.append("Forty");
                         break;
                 }
             }
-
         }
-    return score;
+
+    return score.toString();
+
     }
 }
